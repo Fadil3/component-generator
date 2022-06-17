fcd5e3af1961e0e5f8c7f6c881f8796a1b4ab045
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-06-17 14:58:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-import fs from 'fs'
 import tokenizer from './utils/tokenizer.cjs'
 import parser from './utils/parser.cjs'
-import generator from './utils/generator.cjs'
+import generator from './utils/generator.js'
 
 // retrieve arguments from the command line
 const args = process.argv.slice(2)
 
-// example command : component --name=my-component --function=arrow --styling=module --path=./components/my-component
+// example command : component --name=Button --function=arrow --styling=module --path=./components
 
 const tokens = []
 
@@ -17,6 +16,11 @@ args.forEach((arg) => {
 
 //shift the first token
 const arg1 = tokens.shift()
+
+//throw error if the first token is not a component
+if (arg1.type !== 'name' && arg1.value !== 'component') {
+  throw new Error('The first argument must be a component name')
+}
 
 const parsedToken = parser(tokens, arg1)
 
@@ -31,36 +35,10 @@ parsedToken.arguments.forEach((arg) => {
     state.path = arg.value
   }
   if (arg.type === 'function') {
-    state.function = arg.value
+    state.func = arg.value
   }
   if (arg.type === 'styling') {
     state.styling = arg.value
   }
 })
-console.log(generator(state))
-
-// check if path is valid
-const checkPath = (path) => {
-  // check if path doesn't exist
-  if (!fs.existsSync(path)) {
-    // create the path recursively
-    fs.mkdirSync(path, { recursive: true })
-    console.log('Path created')
-  } else {
-    createFile(state.name, state.style, state.path)
-  }
-}
-
-// create a file with the name of the component
-const createFile = (name, style, path) => {
-  const fileName = `${name}.js`
-  const filePath = `${path}/${fileName}`
-  const fileContent = `import React from 'react'\n
-  import './${name}.css'\n\nexport default () => (\n<div className="${style}">\n${name}\n  </div>\n)\n`
-
-  // write the file
-  fs.writeFileSync(filePath, fileContent)
-  // console.log(`File ${fileName} created`)
-}
-
-// checkPath(path)
+generator(state)
